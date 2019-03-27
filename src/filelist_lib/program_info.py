152e0c97cb747727d46d0e32e2745bd73cc26cc7
Emil Karlén <emil.karlen@fripost.org>
2019-03-27 17:34:54 +0100

    Moves implementation to separate library "filelist_lib"

---
+++ src/filelist_lib/program_info.py
@@ -0,0 +1,7 @@
+VERSION_MAJOR = "0"
+VERSION_MINOR = "9"
+VERSION_PATCH = "0"
+
+VERSION_STRING = ".".join([VERSION_MAJOR,
+                           VERSION_MINOR,
+                           VERSION_PATCH])
